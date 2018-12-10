11948bc1bae815336517a5c6bdd6a6d1a6648057
Diegosg307 <44782483+Diegosg307@users.noreply.github.com>
2018-12-10 09:50:29 -0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,26 @@
+#This file was created by Diego Saldana
+TITLE = " SUPREME JUMP "
+TITLE2 = " GAME OVER FOO :( "
+# screen dims
+WIDTH = 480
+HEIGHT = 600
+# frames per second
+FPS = 60
+# player settings
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.8
+PLAYER_JUMP = 100
+FONT_NAME = 'arcade'
+# platform settings
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (65, HEIGHT - 300, WIDTH-400, 40),
+                 (20, HEIGHT - 350, WIDTH-300, 40),
+                 (200, HEIGHT - 150, WIDTH-350, 40),
+                 (200, HEIGHT - 450, WIDTH-350, 40)]
+#colors
+WHITE = (255, 255, 255)
+GREY = (205,201,201)
+BLUE = (175,238,238)
+BLACK = (0,0,0)
+RED = (178,34,34)
